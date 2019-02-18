2fbb41a14224daf336dffe1a6827b092770929f2
James Cleveland <jc@blit.cc>
2019-02-18 16:35:40 +0000

    remove material stuff

--- .hyper.js
+++ .hyper.js
@@ -92,23 +92,6 @@ module.exports = {
       lightWhite: "#FFFFFF"
     },
 
-    MaterialTheme: {
-      // Set the theme variant,
-      // OPTIONS: 'Darker', 'Palenight', 'Ocean', ''
-      theme: 'Darker',
-
-      // [Optional] Set the rgba() app background opacity, useful when enableVibrance is true
-      // OPTIONS: From 0.1 to 1
-      backgroundOpacity: '1',
-
-      // [Optional] Set the accent color for the current active tab
-      accentColor: '#64FFDA',
-
-      // [Optional] Mac Only. Need restart. Enable the vibrance and blurred background
-      // OPTIONS: 'dark', 'ultra-dark', 'bright'
-      // NOTE: The backgroundOpacity should be between 0.1 and 0.9 to see the effect.
-      vibrancy: 'dark'
-    },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
@@ -164,6 +147,7 @@ module.exports = {
     'hyper-alt-click',
     // 'hyper-captain-sweetheart',
     'hyper-vscode-dark',
+    // 'nord-hyper',
   ],
 
   // in development, you can create a directory under
