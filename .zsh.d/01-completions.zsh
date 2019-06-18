11407025a7f3af3b6a8da419dd2d00d83e94a15a
James Cleveland <jc@blit.cc>
2019-06-18 15:52:21 +0100

    fix: some small fixes and tweaks

--- .zsh.d/01-completions.zsh
+++ .zsh.d/01-completions.zsh
@@ -2,7 +2,7 @@
 
 # completions
 typeset -i updated_at=$(date +'%j' -r ~/.zcompdump 2>/dev/null || stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null)
-if [ $(date +'%j') != $updated_at ]; then
+if [ "$(date +'%j')" != "$updated_at" ]; then
   compinit -i
 else
   compinit -C -i
--- .zsh.d/03-alias.zsh
+++ .zsh.d/03-alias.zsh
@@ -1,10 +1,11 @@
 #!/usr/local/bin zsh
 
 # aliases
-alias 'youtube-dl=noglob youtube-dl '
-alias 'curl=noglob curl '
-alias 'http=noglob http '
-alias 'ip=ip -c -br '
-alias 'brewski=brew update && brew upgrade && brew cleanup; brew doctor '
-alias 'zoom=sudo launchctl remove com.cylance.agent_service'
+alias youtube-'dl=noglob youtube-dl '
+alias curl='noglob curl '
+alias http='noglob http '
+alias ip='ip -c -br '
+alias brewski='brew update && brew upgrade && brew cleanup; brew doctor '
+alias zoom='sudo launchctl remove com.cylance.agent_service'
+alias szrc='source ~/.zshrc'
 
