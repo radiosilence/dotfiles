3d2bbe9fb8733c31acd3afd43568dd105105f3fb
James Cleveland <james.cleveland@zopa.com>
2019-06-18 17:51:31 +0100

    feat: more yarn aliases

--- .zsh.d/yarn.zsh
+++ .zsh.d/yarn.zsh
@@ -4,3 +4,9 @@ alias y='yarn '
 
 alias yt='yarn test '
 alias ytu='yarn test -u '
+alias yr='yarn remove '
+alias ya='yarn add '
+alias yaW='yarn add -W '
+alias yaD='yarn add -D '
+alias yaDW='yarn add -DW '
+
