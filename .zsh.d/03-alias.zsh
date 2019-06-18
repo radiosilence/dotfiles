5ee203d851928bcded8d0093ca161a233775ceea
James Cleveland <jc@blit.cc>
2019-06-18 15:45:58 +0100

    Add .d

---
+++ .zsh.d/03-alias.zsh
@@ -0,0 +1,10 @@
+#!/usr/local/bin zsh
+
+# aliases
+alias 'youtube-dl=noglob youtube-dl '
+alias 'curl=noglob curl '
+alias 'http=noglob http '
+alias 'ip=ip -c -br '
+alias 'brewski=brew update && brew upgrade && brew cleanup; brew doctor '
+alias 'zoom=sudo launchctl remove com.cylance.agent_service'
+
