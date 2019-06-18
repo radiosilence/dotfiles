7c186ff89985c8904965ef827798a269feb555e6
James Cleveland <james.cleveland@zopa.com>
2019-06-18 16:25:55 +0100

    feat: faster k by default

--- .zsh.d/03-alias.zsh
+++ .zsh.d/03-alias.zsh
@@ -8,4 +8,5 @@ alias ip='ip -c -br '
 alias brewski='brew update && brew upgrade && brew cleanup; brew doctor '
 alias zoom='sudo launchctl remove com.cylance.agent_service'
 alias szrc='source ~/.zshrc'
+alias k='k --no-vcs '
 
