557664534eaacae9db22bec6b39479092efd9877
James Cleveland <jc@blit.cc>
2019-07-19 13:54:59 +0100

    feat: dco

--- .zsh.d/fzf.zsh
+++ .zsh.d/fzf.zsh
@@ -12,6 +12,16 @@ fco() {
 }
 
 
+# fco - merge git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+fm() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git merge $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+
 # fco_preview - checkout git branch/tag, with a preview showing the commits between the tag/branch and HEAD
 fcop() {
   local tags branches target
