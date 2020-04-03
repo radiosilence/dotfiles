a6cec55b0d56069c86cb923ed998326bbd26d200
James Cleveland <jc@blit.cc>
2020-04-03 14:06:27 +0100

    feat: trying lean

--- .zsh.d/06-prompt.zsh
+++ .zsh.d/06-prompt.zsh
@@ -2,3 +2,9 @@
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+# Default prompt symbol.
+typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION='→'
+# Prompt symbol in command vi mode.
+typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='←'
+# Prompt symbol in visual vi mode is the same as in command mode.
+typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION='←'
