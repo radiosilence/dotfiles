2d277757c66bdb1a03b651f848d8998ca8ff2bd1
James Cleveland <jc@blit.cc>
2020-04-03 14:01:28 +0100

    chore: remove broken config

--- .zsh.d/06-prompt.zsh
+++ .zsh.d/06-prompt.zsh
@@ -2,10 +2,3 @@
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-# powerlevel9k
-POWERLEVEL10K_DIR_DEFAULT_FOREGROUND='grey58'
-POWERLEVEL10K_DIR_HOME_SUBFOLDER_FOREGROUND='grey58'
-POWERLEVEL10K_VCS_MODIFIED_FOREGROUND='grey42'
-POWERLEVEL10K_VCS_CLEAN_FOREGROUND='grey42'
-
