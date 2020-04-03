6d42d1e59597174ad8757955c54d2390f26c495b
James Cleveland <jc@blit.cc>
2020-04-03 14:46:26 +0100

    fix: tmux thing

--- install.zsh
+++ install.zsh
@@ -7,7 +7,10 @@ cd "${0%/*}" || exit
 for file in .*; do
 	[[ $file == *.git* || $file = "." || $file = ".." || $file = ".vscode" || $file == ".sonarlint" ]] && continue
 	[[ -f ~/$file ]] && unlink ~/$file
-	[[ -v SSH_TTY ]] && [[ $file = ".tmux.conf "]] && echo 'running over ssh, skipping tmux' && continue
+	if [ -v SSH_TTY ] && [ $file = ".tmux.conf "]; then
+		echo "skipping .tmux.conf because on ssh"
+		continue
+	fi
 	echo "linking $file -> ~/$file"
 	[[ ! -d ~/$file ]] && ln -s "$PWD/$file" ~/"$file"
 done
