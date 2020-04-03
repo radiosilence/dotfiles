4cb66cd03c2452cbc3f37a30c9bf292f8822944c
James Cleveland <jc@blit.cc>
2020-04-03 14:17:56 +0100

    fix: context back up top

--- .p10k.zsh
+++ .p10k.zsh
@@ -32,11 +32,11 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
+    context
     dir                     # current directory
     vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
-    context
     prompt_char             # prompt symbol
   )
 
