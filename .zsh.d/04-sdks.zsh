34cba2e9fa8ade683a9416e249c191f1650cc88b
James Cleveland <jc@blit.cc>
2020-04-09 11:58:39 +0100

    fix: asdf properly

--- .zsh.d/04-sdks.zsh
+++ .zsh.d/04-sdks.zsh
@@ -8,9 +8,9 @@ export SDKMAN_DIR=~/.sdkman
 [[ -s ~/.sdkman/bin/sdkman-init.sh ]] && source ~/.sdkman/bin/sdkman-init.sh
 
 # asdf
-# if [[ -d "~/.asdf" ]]; then
+if [[ -d ~/.asdf ]]; then
   source ~/.asdf/asdf.sh
   source ~/.asdf/completions/asdf.bash
-# fi
+fi
 
 [[ -x /usr/libexec/java_home ]] && export JAVA_HOME=$(/usr/libexec/java_home)
