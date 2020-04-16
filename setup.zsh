a46ad3fba84ad0b8736cac8a88aa678d1b9fdd5e
James Cleveland <jc@blit.cc>
2020-04-16 16:57:24 +0100

    feat: fixes and setup

---
+++ setup.zsh
@@ -0,0 +1,47 @@
+#!/usr/bin/env zsh
+brew install \
+  aria2 \
+  asdf \
+  autoconf \
+  automake \
+  bash \
+  cmatrix \
+  coreutils \
+  ffmpeg \
+  findutils \
+  fzf \
+  git \
+  gnu-getopt \
+  gnu-indent \
+  gnu-tar \
+  gnupg \
+  gnutls \
+  nmap \
+  rsync \
+  telnet \
+  tig \
+  tmux \
+  tree \
+  unrar \
+  youtube-dl
+
+# remove brave
+brew cask install \
+  1password \
+  alacritty \
+  android-sdk \
+  brave-browser \
+  fastlane \
+  flipper \
+  google-cloud-sdk \
+  mpv \
+  react-native-debugger \
+  reactotron \
+  ripcord \
+  signal \
+  spotify \
+  telegram \
+  visual-studio-code \
+  whatsapp \
+  zoomus
+
