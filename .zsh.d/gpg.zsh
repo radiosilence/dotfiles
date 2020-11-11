ee499ccd7b8f8ff7ae46ccb922474c9fdc62ef73
James Cleveland <jc@blit.cc>
2020-11-11 11:43:03 +0000

    fix gpg

--- .zsh.d/gpg.zsh
+++ .zsh.d/gpg.zsh
@@ -1,14 +1,18 @@
-gpg-connect-agent --quiet /bye >/dev/null 2>/dev/null
-gpg-agent --daemon --quiet --enable-ssh-support >/dev/null 2>&1
-
-export GPG_TTY=$(tty)
-
 enable_gpg_ssh() {
   export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 }
 
-enable_ssh_agent() {
+enable_ssh_ssh() {
   eval $(ssh-agent)
 }
 
-enable_gpg_ssh
+if command -v gpg &>/dev/null; then
+  gpg-connect-agent --quiet /bye >/dev/null 2>/dev/null
+  gpg-agent --daemon --quiet --enable-ssh-support >/dev/null 2>&1
+
+  export GPG_TTY=$(tty)
+
+  enable_gpg_ssh
+else
+  enable_ssh_ssh
+fi
