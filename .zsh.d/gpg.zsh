f4e6982c29f9671d93228189711bb8c36cef8d74
James Cleveland <jc@blit.cc>
2020-11-11 10:39:00 +0000

    switch between gpg and ssh

--- .zsh.d/gpg.zsh
+++ .zsh.d/gpg.zsh
@@ -1,5 +1,14 @@
 gpg-connect-agent --quiet /bye >/dev/null 2>/dev/null
 gpg-agent --daemon --quiet --enable-ssh-support >/dev/null 2>&1
 
-export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 export GPG_TTY=$(tty)
+
+enable_gpg_ssh() {
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+}
+
+enable_ssh_agent() {
+  eval $(ssh-agent)
+}
+
+enable_gpg_ssh
