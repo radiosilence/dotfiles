3ba40138bebb1da81ca483e2f42ad0c1c9f4ba9d
James Cleveland <jc@blit.cc>
2020-11-13 12:34:03 +0000

    conditional this

--- .zsh.d/nix.zsh
+++ .zsh.d/nix.zsh
@@ -1,2 +1,4 @@
 if [ -e /Users/jc/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/jc/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
-eval "$(direnv hook zsh)"
+if command -v direnv &>/dev/null; then
+  eval "$(direnv hook zsh)"
+fi
