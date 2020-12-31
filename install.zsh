aea70307fc4f15bdad27ff0028cdaa6ea21dbe04
James Cleveland <jc@blit.cc>
2020-12-31 16:31:24 +0000

    make install less destructive

--- install.zsh
+++ install.zsh
@@ -5,22 +5,7 @@ cd "${0%/*}" || exit
 DOTFILES=$(pwd)
 
 echo "export DOTFILES=$DOTFILES" >./.dotfiles-dir
-
-for file in .*; do
-	[[ $file == *.git* || $file = "." || $file = ".." || $file = ".vscode" || $file == ".sonarlint" ]] && continue
-	[[ -f ~/$file ]] && unlink ~/$file
-	if [ -v SSH_TTY ] && [ $file = ".tmux.conf" ]; then
-		echo "skipping .tmux.conf because on ssh"
-		continue
-	fi
-	echo "linking $file -> ~/$file"
-	[[ ! -d ~/$file ]] && ln -s "$PWD/$file" ~/"$file"
-done
-
-antibody bundle <~/.zsh-plugins >~/.zsh-plugins.sh
-
-mkdir -p ~/.config/kitty
-[[ -f ~/.config/kitty/kitty.conf ]] && unlink ~/.config/kitty/kitty.conf
-ln -s "$DOTFILES/kitty.conf" ~/.config/kitty/kitty.conf
+. ./.zsh.d/install.zsh
+install_dotfiles $DOTFILES
 
 cd - || exit
