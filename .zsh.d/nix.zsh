45c0df382a21b9395cb0346ad251f672ccad6bf2
James Cleveland <jc@blit.cc>
2020-12-31 16:41:36 +0000

    fix nix stuff

--- .zsh.d/nix.zsh
+++ .zsh.d/nix.zsh
@@ -1,4 +1,8 @@
-if [ -e /Users/jc/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/jc/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
+  . ~/.nix-profile/etc/profile.d/nix.sh
+
+fi
+
 if is_cmd direnv &>/dev/null; then
   eval "$(direnv hook zsh)"
 fi
