048dd9ed2057116c31497dc906f9e08916c021fa
James Cleveland <jc@blit.cc>
2020-12-31 16:18:24 +0000

    clearning up path

--- .zsh.d/01-path.zsh
+++ .zsh.d/01-path.zsh
@@ -7,17 +7,20 @@ PATH="/usr/local/bin:$PATH"
 PATH="/usr/local/sbin:$PATH"
 PATH="$HOME/.local/bin:$PATH"
 PATH="$DOTFILES/bin:$PATH"
-PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
-PATH="$HOME/Library/Android/sdk/tools/bin:$PATH"
-PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
 PATH="$HOME/.fastlane/bin:$PATH"
 PATH="$HOME/.cargo/bin:$PATH"
+PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
-is_cmd ruby && PATH="$(ruby -e 'print "%s/bin:%s/bin" % [Gem.user_dir, Gem.dir]'):$PATH"
+if is_cmd ruby; then
+  PATH="$(ruby -e 'print "%s/bin:%s/bin" % [Gem.user_dir, Gem.dir]'):$PATH"
+fi
 
-PATH="$HOME/Library/Python/2.7/bin:$PATH"
-
-PATH="$HOME/.cargo/bin:$PATH"
+if is_macos; then
+  PATH="$HOME/Library/Python/2.7/bin:$PATH"
+  PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
+  PATH="$HOME/Library/Android/sdk/tools/bin:$PATH"
+  PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
+fi
 
 # export path
 export PATH
