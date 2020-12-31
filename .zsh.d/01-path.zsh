c7f2c61b99c812acce67b76f67bbac402da2632d
James Cleveland <jc@blit.cc>
2020-12-31 16:56:56 +0000

    rearrange thses

--- .zsh.d/01-path.zsh
+++ .zsh.d/01-path.zsh
@@ -1,15 +1,6 @@
 # path
-PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
-PATH="/usr/local/opt/uutils-coreutils/libexec/uubin:$PATH"
-PATH="/usr/local/opt/gnu-getopt/bin:$PATH"
 PATH="/usr/local/bin:$PATH"
 PATH="/usr/local/sbin:$PATH"
-PATH="$HOME/.local/bin:$PATH"
-PATH="$DOTFILES/bin:$PATH"
-PATH="$HOME/.fastlane/bin:$PATH"
-PATH="$HOME/.cargo/bin:$PATH"
-PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 if is_cmd ruby; then
   PATH="$(ruby -e 'print "%s/bin:%s/bin" % [Gem.user_dir, Gem.dir]'):$PATH"
@@ -20,7 +11,21 @@ if is_macos; then
   PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
   PATH="$HOME/Library/Android/sdk/tools/bin:$PATH"
   PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
+  PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+  PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
+  PATH="/usr/local/opt/uutils-coreutils/libexec/uubin:$PATH"
+  PATH="/usr/local/opt/gnu-getopt/bin:$PATH"
 fi
+
+PATH="$HOME/.local/bin:$PATH"
+PATH="$DOTFILES/bin:$PATH"
+PATH="$HOME/.fastlane/bin:$PATH"
+
+if is_cmd cargo; then
+  PATH="$HOME/.cargo/bin:$PATH"
+fi
+
+PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # export path
 export PATH
