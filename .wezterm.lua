ccde717ac8a16ef53545de45f262e3433cd9667a
James Cleveland <jc@blit.cc>
2021-04-01 13:16:39 +0100

    some wezterm and kitty stuff

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,4 +2,19 @@ local wezterm = require 'wezterm';
 
 return {
    font = wezterm.font("Iosevka Nerd Font"),
+   font_rules = {
+      {
+         intensity = "Bold",
+         font = wezterm.font("Iosevka", {bold=true}),
+      },
+      {
+         italic = true,
+         intensity = "Bold",
+         font = wezterm.font("Iosevka", {bold=true, italic=true}),
+      },
+      {
+         italic = true,
+         font = wezterm.font("Iosevka", {italic=true}),
+      }
+   }
 }
