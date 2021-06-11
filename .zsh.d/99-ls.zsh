3b5c5d26ca6c7d66b47e474059797c2da7d8264b
James Cleveland <jc@blit.cc>
2021-06-11 13:53:21 +0100

    fixes for m1

--- .zsh.d/99-ls.zsh
+++ .zsh.d/99-ls.zsh
@@ -1,77 +1,77 @@
+alias 'll=ls -lh '
 
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
-local _DIRNAME="${0:h}"
+# 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+# local _DIRNAME="${0:h}"
 
-if (( $+commands[exa] )); then
-  typeset -g exa_params
-  # Use exa
-  exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
+# if (( $+commands[exa] )); then
+#   typeset -g exa_params
+#   # Use exa
+#   exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
 
-  function ls(){
-    exa ${exa_params} $@
-  }
-  compdef ls=exa
+#   function ls(){
+#     exa ${exa_params} $@
+#   }
+#   compdef ls=exa
 
-  function l(){
-    exa --git-ignore ${exa_params} $@
-  }
-  compdef l=exa
+#   function l(){
+#     exa --git-ignore ${exa_params} $@
+#   }
+#   compdef l=exa
 
-  function la(){
-    exa -a ${exa_params} $@
-  }
-  compdef la=exa
+#   function la(){
+#     exa -a ${exa_params} $@
+#   }
+#   compdef la=exa
 
-  function ll(){
+#   function ll(){
 
-    exa --header --long ${exa_params} $@
-  }
-  compdef ll=exa
-else
-  typeset -g _ls
-  _ls=(=ls)
+#     exa --header --long ${exa_params} $@
+#   }
+#   compdef ll=exa
+# else
+#   typeset -g _ls
+#   _ls=(=ls)
 
-  if (( $+commands[gls] )); then
-    _ls=(=gls)
-  fi
+#   if (( $+commands[gls] )); then
+#     _ls=(=gls)
+#   fi
 
-  typeset -g _ls_params
-  _ls_params=('-hF' '--group-directories-first' '--time-style=+%Y-%m-%d %H:%M' '--quoting-style=literal')
+#   typeset -g _ls_params
+#   _ls_params=('-hF' '--group-directories-first' '--time-style=+%Y-%m-%d %H:%M' '--quoting-style=literal')
 
-  if ${_ls[@]} --hyperlink >/dev/null 2>&1 ; then
-    _ls_params+=('--hyperlink')
-  fi
+#   if ${_ls[@]} --hyperlink >/dev/null 2>&1 ; then
+#     _ls_params+=('--hyperlink')
+#   fi
 
-  if [[ "$CLICOLOR" != "0" ]]; then
-    _ls_params+=('--color')
-  fi
+#   if [[ "$CLICOLOR" != "0" ]]; then
+#     _ls_params+=('--color')
+#   fi
 
-  if (( $+commands[grc] )); then
-    _grc=('grc' "--config=${_DIRNAME}/conf.ls" )
-  fi
+#   if (( $+commands[grc] )); then
+#     _grc=('grc' "--config=${_DIRNAME}/conf.ls" )
+#   fi
 
-  function ls(){
-    $_ls ${_ls_params} -C $@
-  }
-  compdef ls=ls
+#   function ls(){
+#     $_ls ${_ls_params} -C $@
+#   }
+#   compdef ls=ls
 
-  function l(){
-    $_ls ${_ls_params} -C $@
-  }
-  compdef l=ls
+#   function l(){
+#     $_ls ${_ls_params} -C $@
+#   }
+#   compdef l=ls
 
-  function la(){
-    $_ls ${_ls_params} -C -A $@
-  }
-  compdef la=ls
+#   function la(){
+#     $_ls ${_ls_params} -C -A $@
+#   }
+#   compdef la=ls
 
-  function ll(){
-    if [[ "$CLICOLOR" != "0" ]]; then
-      $_grc $_ls ${_ls_params} -l $@
-    else
-      $_ls -l $@
-    fi
-  }
-  compdef ll=ls
-fi
-
+#   function ll(){
+#     if [[ "$CLICOLOR" != "0" ]]; then
+#       $_grc $_ls ${_ls_params} -l $@
+#     else
+#       $_ls -l $@
+#     fi
+#   }
+#   compdef ll=ls
+# fi
