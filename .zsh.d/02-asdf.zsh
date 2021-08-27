5f575020b254e55899c1da9a2cf603ca6b82252d
James Cleveland <jc@blit.cc>
2021-08-27 18:46:22 +0100

    fixes for ytdl and shit

--- .zsh.d/02-asdf.zsh
+++ .zsh.d/02-asdf.zsh
@@ -1,10 +1,8 @@
 # asdf
 if [ -f ~/.asdf/asdf.sh ]; then
   . ~/.asdf/asdf.sh
-  fpath=(${ASDF_DIR}/completions $fpath)
 fi
 
-if [ -f /opt/homebrew/opt/asdf/asdf.sh ]; then
-  . /opt/homebrew/opt/asdf/asdf.sh
-  fpath=(${ASDF_DIR}/completions $fpath)
+if [ -d /opt/homebrew/opt/asdf ]; then
+  . /opt/homebrew/opt/asdf/libexec/asdf.sh
 fi
--- .zsh.d/youtube-dl.zsh
+++ .zsh.d/youtube-dl.zsh
@@ -1,1 +1,7 @@
-alias ytdl='youtube-dl '
+if is_cmd yt-dlp; then
+  alias ytdl='yt-dlp'
+elif is_cmd youtube-dlc; then
+  alias ytdl='youtube-dlc'
+elif is_cmd youtube-dl; then
+  alias ytdl='youtube-dl'
+fi
