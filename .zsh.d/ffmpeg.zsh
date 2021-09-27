b101bc107ad2fe82ca22f98928b83b740cb2f953
James Cleveland <jc@blit.cc>
2021-09-27 16:34:07 +0100

    remove some croteutyylslusiolls

---
+++ .zsh.d/ffmpeg.zsh
@@ -0,0 +1,9 @@
+ff2mp4() {
+  ffmpeg -find_stream_info -i $1 \
+    -map 0 -c:v copy -c:a copy -c:s mov_text {1:r}.mp4 \
+    -hide_banner -loglevel warning
+}
+
+ffprobesubs() {
+  ffprobe -v error -of json $1 -of json -show_entries stream -select_streams s
+}
