430a4e508402790319de68f46e71ecb8fe13db29
James Cleveland <jc@blit.cc>
2021-09-29 17:03:40 +0100

    fixes for setup script

--- setup-macos.zsh
+++ setup-macos.zsh
@@ -7,7 +7,7 @@ if ! is_cmd brew; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if [ !-d ~/.zgenon ]; then
+if [ ! -d ~/.zgenom ]; then
   git clone https://github.com/jandamm/zgenom.git ~/.zgenom
 fi
 
@@ -34,7 +34,9 @@ brew install \
   zoxide \
   zoxide
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if ! is_cmd cargo; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
 
 cargo install \
   fcp
