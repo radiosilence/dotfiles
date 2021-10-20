831e65d2c7ce02045f4512de0e9598e464fa91ec
James Cleveland <jc@blit.cc>
2021-10-20 16:54:42 +0100

    make it a bit less in yer face

---
+++ .zsh.d/04-zsh-autocomplete.zsh
@@ -0,0 +1,15 @@
+zstyle ':autocomplete:*' fzf-completion yes
+zstyle ':autocomplete:*' min-delay 1.0
+
+zstyle ':autocomplete:*' default-context ''
+# '': Start each new command line with normal autocompletion.
+# history-incremental-search-backward: Start in live history search mode.
+
+zstyle ':autocomplete:*' recent-dirs zoxide
+# cdr:  Use Zsh's `cdr` function to show recent directories as completions.
+# no:   Don't show recent directories.
+# zsh-z|zoxide|z.lua|z.sh|autojump|fasd: Use this instead (if installed).
+# ⚠️ NOTE: This setting can NOT be changed at runtime.
+
+zle -A {.,}history-incremental-search-forward
+zle -A {.,}history-incremental-search-backward
---
+++ .zsh.d/06-zsh-autocomplete.zsh
@@ -0,0 +1,5 @@
+bindkey '\e[A' up-line-or-history
+bindkey '\eOA' up-line-or-history
+
+bindkey '\e[B' down-line-or-history
+bindkey '\eOB' down-line-or-history
