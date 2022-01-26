7617f79135ce99d056e2a1618b55f0e94751cd7b
James Cleveland <jc@blit.cc>
2022-01-26 10:54:19 +0000

    add untested music import

--- .zsh.d/music.zsh
+++ .zsh.d/music.zsh
@@ -1,6 +1,14 @@
 flac2alac() {
   for i in *.flac; do
-    echo $i
+    echo $i → "${i%.flac}".m4a
     ffmpeg -i "$i" -y -v 0 -vcodec copy -acodec alac "${i%.flac}".m4a && rm -f "$i"
   done
+}
+
+importmusic() {
+  (cd $1 && flac2alac)
+  ssh music@soul "rm -rf ~/inbox/*"
+  rsync -av $1 music@soul:inbox/
+  ssh music@soul "beet import ~/inbox/*"
+  (cd /Volumes/Stanley && ./pull.sh)
 }
