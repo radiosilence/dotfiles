4c4f49391a32b65ae892f6546cedf7b492be7f05
James Cleveland <james.cleveland@engineerbetter.com>
2022-04-29 13:25:39 +0100

    nicer formatting

--- .zsh.d/bat.zsh
+++ .zsh.d/bat.zsh
@@ -1,6 +1,10 @@
 if is_cmd bat; then
-  alias "bat=bat --map-syntax='*.kubeconfig:YAML'"
+  alias "bat=bat \
+    --map-syntax='*.kubeconfig:YAML' \
+  "
   alias "cat=bat"
   # alias "fd=fd -X bat"
-  alias "fzf=fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+  alias "fzf=fzf \
+    --preview 'bat --color=always --style=numbers --line-range=:500 {}' \
+  "
 fi
