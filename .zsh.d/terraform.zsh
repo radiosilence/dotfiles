c9937ba498508205498fd48cc1d126f02061711e
James Cleveland <jc@blit.cc>
2023-01-11 13:58:48 +0000

    terraform fix

--- .zsh.d/terraform.zsh
+++ .zsh.d/terraform.zsh
@@ -1,3 +1,2 @@
-
 autoload -U +X bashcompinit && bashcompinit
-complete -o nospace -C /opt/homebrew/Cellar/tfenv/2.2.3/versions/1.1.9/terraform terraform
+complete -o nospace -C "$BREW_PREFIX/bin/terraform" terraform
