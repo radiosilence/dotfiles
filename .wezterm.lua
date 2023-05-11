d8b92948d814fda8f8e715b43c23f0bdfee0260e
James Cleveland <jc@blit.cc>
2023-05-11 14:47:10 +0100

    Various changes to work nicely with helix

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,91 +1,107 @@
 local wezterm = require 'wezterm';
 
 return {
-    initial_rows = 64,
-    initial_cols = 240,
-    font = wezterm.font("Hack Nerd Font JBM Ligatured CCG"),
-    font_rules = {
-        {
-            intensity = "Bold",
-            font = wezterm.font("Hack Nerd Font JBM Ligatured CCG", { bold = true })
-        }, {
-            italic = true,
-            intensity = "Bold",
-            font = wezterm.font("Hack Nerd Font JBM Ligatured CCG",
-                { bold = true, italic = true })
-        },
-        {
-            italic = true,
-            font = wezterm.font("Hack Nerd Font JBM Ligatured CCG", { italic = true })
-        }
+  initial_rows = 64,
+  initial_cols = 240,
+  font = wezterm.font("Hack Nerd Font JBM Ligatured CCG"),
+  font_rules = {
+    {
+      intensity = "Bold",
+      font = wezterm.font("Hack Nerd Font JBM Ligatured CCG", { bold = true })
+    }, {
+    italic = true,
+    intensity = "Bold",
+    font = wezterm.font("Hack Nerd Font JBM Ligatured CCG",
+      { bold = true, italic = true })
+  },
+    {
+      italic = true,
+      font = wezterm.font("Hack Nerd Font JBM Ligatured CCG", { italic = true })
+    }
+  },
+  default_prog = {
+    "sh", "-c",
+    "export PATH=\"$PATH:/opt/homebrew/bin:/usr/local/bin/:$HOME/.homebrew/bin\"; which tmux && (tmux a || tmux) || zsh"
+  },
+  -- default_prog = {"zsh"},
+  send_composed_key_when_left_alt_is_pressed = false,
+  send_composed_key_when_right_alt_is_pressed = true,
+  keys = {
+    -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+    {
+      key = "LeftArrow",
+      mods = "OPT",
+      action = { SendKey = { key = "b", mods = "ALT" } }
     },
-    default_prog = {
-        "sh", "-c",
-        "export PATH=\"$PATH:/opt/homebrew/bin:/usr/local/bin/:$HOME/.homebrew/bin\"; which tmux && (tmux a || tmux) || zsh"
+    {
+      key = "RightArrow",
+      mods = "OPT",
+      action = { SendKey = { key = "f", mods = "ALT" } }
     },
-    -- default_prog = {"zsh"},
-    send_composed_key_when_left_alt_is_pressed = false,
-    send_composed_key_when_right_alt_is_pressed = true,
-    keys = {
-        -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
-        {
-            key = "LeftArrow",
-            mods = "OPT",
-            action = { SendKey = { key = "b", mods = "ALT" } }
-        }, {
-            key = "RightArrow",
-            mods = "OPT",
-            action = { SendKey = { key = "f", mods = "ALT" } }
-        }
+    {
+      key = "3",
+      mods = "ALT",
+      action = { SendKey = { key = "#" } }
     },
-    colors = {
-        -- The default text color
-        foreground = "#f7f1ff",
-        -- The default background color
-        background = "#221f22",
-
-        -- Overrides the cell background color when the current cell is occupied by the
-        -- cursor and the cursor style is set to Block
-        cursor_bg = "#52ad70",
-        -- Overrides the text color when the current cell is occupied by the cursor
-        cursor_fg = "black",
-        -- Specifies the border color of the cursor when the cursor style is set to Block,
-        -- of the color of the vertical or horizontal bar when the cursor style is set to
-        -- Bar or Underline.
-        cursor_border = "#52ad70",
-
-        -- the foreground color of selected text
-        selection_fg = "white",
-        -- the background color of selected text
-        selection_bg = "#314f78",
-
-        -- The color of the scrollbar "thumb"; the portion that represents the current viewport
-        scrollbar_thumb = "#222222",
-
-        -- The color of the split lines between panes
-        split = "#444444",
-        brights = {
-            "#b195b1",
-            "#ff5c8a",
-            "#55fc79",
-            "#ffe342",
-            "#61efff",
-            "#8e80ff",
-            "#4dffff",
-            "#ffffff",
-        },
-        ansi = {
-            "#221f22",
-            "#fc618d",
-            "#7bd88f",
-            "#fce566",
-            "#78DCE8",
-            "#948ae3",
-            "#5ad4e6",
-            "#f7f1ff",
-        },
+    {
+      key = "2",
+      mods = "ALT",
+      action = { SendKey = { key = "€" } }
     },
-    hide_tab_bar_if_only_one_tab = true,
-    window_padding = { left = 2, right = 2, top = 2, bottom = 2 },
-    window_close_confirmation = "NeverPrompt"
+    {
+      key = "0",
+      mods = "ALT",
+      action = { SendKey = { key = "º" } }
+    },
+  },
+  colors = {
+    -- The default text color
+    foreground = "#f7f1ff",
+    -- The default background color
+    background = "#221f22",
+
+    -- Overrides the cell background color when the current cell is occupied by the
+    -- cursor and the cursor style is set to Block
+    cursor_bg = "#52ad70",
+    -- Overrides the text color when the current cell is occupied by the cursor
+    cursor_fg = "black",
+    -- Specifies the border color of the cursor when the cursor style is set to Block,
+    -- of the color of the vertical or horizontal bar when the cursor style is set to
+    -- Bar or Underline.
+    cursor_border = "#52ad70",
+
+    -- the foreground color of selected text
+    selection_fg = "white",
+    -- the background color of selected text
+    selection_bg = "#314f78",
+
+    -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+    scrollbar_thumb = "#222222",
+
+    -- The color of the split lines between panes
+    split = "#444444",
+    brights = {
+      "#b195b1",
+      "#ff5c8a",
+      "#55fc79",
+      "#ffe342",
+      "#61efff",
+      "#8e80ff",
+      "#4dffff",
+      "#ffffff",
+    },
+    ansi = {
+      "#221f22",
+      "#fc618d",
+      "#7bd88f",
+      "#fce566",
+      "#78DCE8",
+      "#948ae3",
+      "#5ad4e6",
+      "#f7f1ff",
+    },
+  },
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = { left = 2, right = 2, top = 2, bottom = 2 },
+  window_close_confirmation = "NeverPrompt"
 }
