a1589f651b7029cef5cbde29d5c215a8750c6a50
James Cleveland <jc@blit.cc>
2023-09-20 10:24:13 +0100

    get rid of generic sdks

---
+++ .zsh.d/java.zsh
@@ -0,0 +1,16 @@
+try_java() {
+  _JAVA_HOME="/Library/Java/JavaVirtualMachines/$1-$2.jdk/Contents/Home"
+  [[ -d $_JAVA_HOME ]] && export JAVA_HOME=$_JAVA_HOME
+}
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+try_java adoptopenjdk 8
+try_java adoptopenjdk 14
+try_java zulu 11
+try_java zulu 17
+try_java zulu 21
