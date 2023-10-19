952e2d8a889d8c2a44005bdd0695cf15e87ad12c
James Cleveland <jc@blit.cc>
2023-10-19 10:38:58 +0100

    re-add brew stuff?

--- .zsh.d/01-brew.zsh
+++ .zsh.d/01-brew.zsh
@@ -1,14 +1,13 @@
-# if [ -d ~/.homebrew ]; then
-#   export BREW_PREFIX=~/.homebrew
-# elif [ -d /opt/homebrew ]; then
-#   export BREW_PREFIX=/opt/homebrew
-# elif [ -d /usr/local/homebrew ]; then
-#   export BREW_PREFIX=/usr/local/homebrew
-# fi
+if [ -d ~/.homebrew ]; then
+  export BREW_PREFIX=~/.homebrew
+elif [ -d /opt/homebrew ]; then
+  export BREW_PREFIX=/opt/homebrew
+elif [ -d /usr/local/homebrew ]; then
+  export BREW_PREFIX=/usr/local/homebrew
+fi
 
 export HOMEBREW_CASK_OPTS=--no-quarantine
-# export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$PATH"
-# export PATH="$BREW_PREFIX/opt/openssl@3/bin:$PATH"
+export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$PATH"
 
 # fi
 # if is_cmd brew; then
