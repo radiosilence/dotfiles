9d8971224221a98f3fff242092e9eadf9d8c4202
James Cleveland <jc@blit.cc>
2024-04-18 12:33:18 +0100

    disable gpg agent for now

--- .zsh.d/gpg-agent.zsh
+++ .zsh.d/gpg-agent.zsh
@@ -1,10 +1,10 @@
-if is_cmd gpg-connect-agent; then
-  export GPG_TTY=$(tty)
-  gpg-connect-agent updatestartuptty /bye >/dev/null
-  unset SSH_AGENT_PID
-  export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+# if is_cmd gpg-connect-agent; then
+#   export GPG_TTY=$(tty)
+#   gpg-connect-agent updatestartuptty /bye >/dev/null
+#   unset SSH_AGENT_PID
+#   export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 
-  gpg-switch-yubikey() {
-    gpg-connect-agent "scd serialno" "learn --force" /bye
-  }
-fi
+#   gpg-switch-yubikey() {
+#     gpg-connect-agent "scd serialno" "learn --force" /bye
+#   }
+# fi
