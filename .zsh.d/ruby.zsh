5fc53f792dc30da3a246640fa3e1f5b8552194d9
James Cleveland <jc@blit.cc>
2024-05-06 12:40:06 +0100

    use omz plugin for asdf

--- .zsh.d/ruby.zsh
+++ .zsh.d/ruby.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/opt/ruby/bin:$PATH"
-export PATH="/usr/local/lib/ruby/gems/3.3.0/bin:$PATH"
+# export PATH="/usr/local/opt/ruby/bin:$PATH"
+# export PATH="/usr/local/lib/ruby/gems/3.3.0/bin:$PATH"
