b4a092bd21f049a64efd83b0669bd8163372653c
James Cleveland <jc@blit.cc>
2024-05-08 16:51:52 +0100

    finally fix bashcompinit

--- .zsh.d/ops.zsh
+++ .zsh.d/ops.zsh
@@ -1,4 +1,3 @@
 export OPS_DIR="$HOME/.ops"
 export PATH="$HOME/.ops/bin:$PATH"
-# source "$HOME/.ops/scripts/bash_completion.sh"
-autoload bashcompinit
+source "$HOME/.ops/scripts/bash_completion.sh"
