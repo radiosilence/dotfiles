f12917ab05f4b63a2aea5d33695e6f9e96d954dd
James Cleveland <jc@blit.cc>
2024-06-05 19:50:17 +0100

    move fiels into common

--- .zsh.d/00-prelude.zsh
+++ .zsh.d/00-prelude.zsh
@@ -1,18 +1,5 @@
-is_cmd() {
-  command -v $1 &>/dev/null
-}
-
-is_macos() {
-  [ -d /Library ]
-}
-
-join_by() {
-  local d=$1
-  shift
-  local f=$1
-  shift
-  printf %s "$f" "${@/#/$d}"
-}
+. ~/.dotfiles-dir
+. $DOTFILES/common.zsh
 
 # opts
 setopt no_share_history
---
+++ common.zsh
@@ -0,0 +1,15 @@
+is_cmd() {
+  command -v $1 &>/dev/null
+}
+
+is_macos() {
+  [ -d /Library ]
+}
+
+join_by() {
+  local d=$1
+  shift
+  local f=$1
+  shift
+  printf %s "$f" "${@/#/$d}"
+}
