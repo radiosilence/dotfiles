09ea138269e022e3da361005adf9579fe2282436
James Cleveland <jc@blit.cc>
2024-06-12 10:56:37 +0100

    remove quarantine disable again

--- .zsh.d/01-brew.zsh
+++ .zsh.d/01-brew.zsh
@@ -6,9 +6,5 @@ else
   export BREW_PREFIX=/usr/local
 fi
 
-export HOMEBREW_CASK_OPTS=--no-quarantine
 export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$PATH"
 export HOMEBREW_BUNDLE_FILE="~/Brewfile"
-# fi
-# if is_cmd brew; then
-#   FPATH="$BREW_PREFIX/share/zsh/site-functions:${FPATH}"
