b755344d3da849579de340c6789b5fec2cb57a5b
James Cleveland <jc@blit.cc>
2024-06-13 13:10:23 +0100

    more deps

--- .zsh.d/01-brew.zsh
+++ .zsh.d/01-brew.zsh
@@ -8,3 +8,7 @@ fi
 
 export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$PATH"
 export HOMEBREW_BUNDLE_FILE="~/Brewfile"
+
+brew-bundle-install() {
+  echo 'brew "'$1'"' >>$HOME/Brewfile && brew bundle
+}
