54d349d284cb61967ba8a8b7ffe8772890d25ad6
James Cleveland <jc@blit.cc>
2024-07-13 15:26:05 +0100

    mise conditional

--- .zsh.d/01-mise.zsh
+++ .zsh.d/01-mise.zsh
@@ -1,1 +1,7 @@
-eval "$(mise activate zsh)"
+if is_cmd mise; then
+  eval "$(mise activate zsh)"
+elif [ -x ~/.local/bin/mise ]; then
+  eval $(~/.local/bin/mise activate zsh)
+else
+  echo "mise not found"
+fi
