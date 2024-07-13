9a1cc776779d56d83f5ffe0654477fed1d459ce8
James Cleveland <jc@blit.cc>
2024-07-13 15:27:13 +0100

    quote it

--- .zsh.d/01-mise.zsh
+++ .zsh.d/01-mise.zsh
@@ -1,7 +1,7 @@
 if is_cmd mise; then
   eval "$(mise activate zsh)"
 elif [ -x ~/.local/bin/mise ]; then
-  eval $(~/.local/bin/mise activate zsh)
+  eval "$(~/.local/bin/mise activate zsh)"
 else
   echo "mise not found"
 fi
