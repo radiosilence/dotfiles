0889546c5d96d51f3c05936ec350dda1071b9987
James Cleveland <jc@blit.cc>
2024-07-13 15:47:43 +0100

    conditional fzf

--- .zsh.d/04-fzf.zsh
+++ .zsh.d/04-fzf.zsh
@@ -1,143 +1,147 @@
 ## handy stuff
-eval "$(fzf --zsh)"
+if is_cmd fzf; then
 
-# bindkey "ç" fzf-cd-widget
+  eval "$(fzf --zsh)"
 
-export FZF_DEFAULT_OPTS="--layout=reverse --inline-info --height 40% --preview 'cat {}'"
+  # bindkey "ç" fzf-cd-widget
 
-FZF_CMD="fzf"
-FZF_ARGS=""
+  export FZF_DEFAULT_OPTS="--layout=reverse --inline-info --height 40% --preview 'cat {}'"
 
-# fco - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-fco() {
-  local branches branch
-  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-    branch=$(echo "$branches" |
-      $FZF_CMD $FZF_ARGS -d $((2 + $(wc -l <<<"$branches"))) +m) &&
-    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
+  FZF_CMD="fzf"
+  FZF_ARGS=""
 
-# fco - merge git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-fm() {
-  local branches branch
-  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-    branch=$(echo "$branches" |
-      $FZF_CMD $FZF_ARGS -d $((2 + $(wc -l <<<"$branches"))) +m) &&
-    git merge $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
+  # fco - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+  fco() {
+    local branches branch
+    branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+      branch=$(echo "$branches" |
+        $FZF_CMD $FZF_ARGS -d $((2 + $(wc -l <<<"$branches"))) +m) &&
+      git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+  }
 
-# fco_preview - checkout git branch/tag, with a preview showing the commits between the tag/branch and HEAD
-fcop() {
-  local tags branches target
-  branches=$(
-    git --no-pager branch --all --sort=-committerdate \
-      --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" |
-      sed '/^$/d'
-  ) || return
-  tags=$(
-    git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}'
-  ) || return
-  target=$(
-    (
-      echo "$branches"
-      echo "$tags"
-    ) |
-      fzf --no-hscroll --no-multi -n 2 \
-        --ansi --preview="git --no-pager log -150 --pretty=format:%s '..{2}'"
-  ) || return
-  git checkout $(awk '{print $2}' <<<"$target")
-}
+  # fco - merge git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+  fm() {
+    local branches branch
+    branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+      branch=$(echo "$branches" |
+        $FZF_CMD $FZF_ARGS -d $((2 + $(wc -l <<<"$branches"))) +m) &&
+      git merge $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+  }
 
-# fcoc - checkout git commit
-fcoc() {
-  local commits commit
-  commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
-    commit=$(echo "$commits" | fzf --tac +s +m -e) &&
-    git checkout $(echo "$commit" | sed "s/ .*//")
-}
+  # fco_preview - checkout git branch/tag, with a preview showing the commits between the tag/branch and HEAD
+  fcop() {
+    local tags branches target
+    branches=$(
+      git --no-pager branch --all --sort=-committerdate \
+        --format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" |
+        sed '/^$/d'
+    ) || return
+    tags=$(
+      git --no-pager tag | awk '{print "\x1b[35;1mtag\x1b[m\t" $1}'
+    ) || return
+    target=$(
+      (
+        echo "$branches"
+        echo "$tags"
+      ) |
+        fzf --no-hscroll --no-multi -n 2 \
+          --ansi --preview="git --no-pager log -150 --pretty=format:%s '..{2}'"
+    ) || return
+    git checkout $(awk '{print $2}' <<<"$target")
+  }
 
-## ASDF
+  # fcoc - checkout git commit
+  fcoc() {
+    local commits commit
+    commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
+      commit=$(echo "$commits" | fzf --tac +s +m -e) &&
+      git checkout $(echo "$commit" | sed "s/ .*//")
+  }
 
-## BREW
+  ## ASDF
 
-# Install (one or multiple) selected application(s)
-# using "brew search" as source input
-# mnemonic [B]rew [I]nstall [P]lugin
-bip() {
-  local inst=$(brew search | fzf -m)
+  ## BREW
 
-  if [[ $inst ]]; then
-    for prog in $(echo $inst); do
-      brew install $prog
-    done
-  fi
-}
-# Update (one or multiple) selected application(s)
-# mnemonic [B]rew [U]pdate [P]lugin
-bup() {
-  local upd=$(brew leaves | fzf -m)
+  # Install (one or multiple) selected application(s)
+  # using "brew search" as source input
+  # mnemonic [B]rew [I]nstall [P]lugin
+  bip() {
+    local inst=$(brew search | fzf -m)
 
-  if [[ $upd ]]; then
-    for prog in $(echo $upd); do
-      brew upgrade $prog
-    done
-  fi
-}
-# Delete (one or multiple) selected application(s)
-# mnemonic [B]rew [R]lean [P]lugin (e.g. uninstall)
-brp() {
-  local uninst=$(brew leaves | fzf -m)
-
-  if [[ $uninst ]]; then
-    for prog in $(echo $uninst); do
-      brew uninstall $prog
-    done
-  fi
-}
-# Install or open the webpage for the selected application
-# using brew cask search as input source
-# and display a info quickview window for the currently marked application
-bcip() {
-  local token
-  token=$(brew cask search | $FZF_CMD $FZF_ARGS --query="$1" +m --preview 'brew cask info {}')
-
-  if [ "x$token" != "x" ]; then
-    echo "(I)nstall or open the (h)omepage of $token"
-    read input
-    if [ $input = "i" ] || [ $input = "I" ]; then
-      brew cask install $token
+    if [[ $inst ]]; then
+      for prog in $(echo $inst); do
+        brew install $prog
+      done
     fi
-    if [ $input = "h" ] || [ $input = "H" ]; then
-      brew cask home $token
+  }
+  # Update (one or multiple) selected application(s)
+  # mnemonic [B]rew [U]pdate [P]lugin
+  bup() {
+    local upd=$(brew leaves | fzf -m)
+
+    if [[ $upd ]]; then
+      for prog in $(echo $upd); do
+        brew upgrade $prog
+      done
     fi
-  fi
-}
-# Uninstall or open the webpage for the selected application
-# using brew list as input source (all brew cask installed applications)
-# and display a info quickview window for the currently marked application
-bcrp() {
-  local token
-  token=$(brew cask list | $FZF_CMD $FZF_ARGS --query="$1" +m --preview 'brew cask info {}')
+  }
+  # Delete (one or multiple) selected application(s)
+  # mnemonic [B]rew [R]lean [P]lugin (e.g. uninstall)
+  brp() {
+    local uninst=$(brew leaves | fzf -m)
 
-  if [ "x$token" != "x" ]; then
-    echo "(U)ninstall or open the (h)omepage of $token"
-    read input
-    if [ $input = "u" ] || [ $input = "U" ]; then
-      brew cask uninstall $token
+    if [[ $uninst ]]; then
+      for prog in $(echo $uninst); do
+        brew uninstall $prog
+      done
     fi
-    if [ $input = "h" ] || [ $token = "h" ]; then
-      brew cask home $token
+  }
+  # Install or open the webpage for the selected application
+  # using brew cask search as input source
+  # and display a info quickview window for the currently marked application
+  bcip() {
+    local token
+    token=$(brew cask search | $FZF_CMD $FZF_ARGS --query="$1" +m --preview 'brew cask info {}')
+
+    if [ "x$token" != "x" ]; then
+      echo "(I)nstall or open the (h)omepage of $token"
+      read input
+      if [ $input = "i" ] || [ $input = "I" ]; then
+        brew cask install $token
+      fi
+      if [ $input = "h" ] || [ $input = "H" ]; then
+        brew cask home $token
+      fi
     fi
-  fi
-}
+  }
+  # Uninstall or open the webpage for the selected application
+  # using brew list as input source (all brew cask installed applications)
+  # and display a info quickview window for the currently marked application
+  bcrp() {
+    local token
+    token=$(brew cask list | $FZF_CMD $FZF_ARGS --query="$1" +m --preview 'brew cask info {}')
 
-# GIT heart FZF
-# -------------
+    if [ "x$token" != "x" ]; then
+      echo "(U)ninstall or open the (h)omepage of $token"
+      read input
+      if [ $input = "u" ] || [ $input = "U" ]; then
+        brew cask uninstall $token
+      fi
+      if [ $input = "h" ] || [ $token = "h" ]; then
+        brew cask home $token
+      fi
+    fi
+  }
 
-is_in_git_repo() {
-  git rev-parse HEAD >/dev/null 2>&1
-}
+  # GIT heart FZF
+  # -------------
 
-fzf-down() {
-  fzf --height 50% "$@" --border
-}
+  is_in_git_repo() {
+    git rev-parse HEAD >/dev/null 2>&1
+  }
+
+  fzf-down() {
+    fzf --height 50% "$@" --border
+  }
+
+fi
