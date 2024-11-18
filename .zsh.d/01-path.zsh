8e2d01e86ad6ac525e271be81d17a71b38c1f96c
James Cleveland <jc@blit.cc>
2024-11-18 14:26:16 +0000

    some pull push stuff and unfucking mise by puting usr local at end of paths

--- .zsh.d/01-path.zsh
+++ .zsh.d/01-path.zsh
@@ -4,8 +4,7 @@ paths=(
   ~/.fastlane/bin
   ~/.yarn/bin
   ~/.config/yarn/global/node_modules/.bin
-  /usr/local/bin
 )
 
 # export path
-export PATH="$(join_by : $paths):$PATH"
+export PATH="$PATH:$(join_by : $paths):/usr/local/bin"
