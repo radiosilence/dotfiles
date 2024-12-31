adf89c24b86d0218efe56e69842b601f669723eb
James Cleveland <jc@blit.cc>
2024-12-31 12:30:34 +0000

    more editor things

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,2 @@
 fish_add_path -p ~/.dotfiles/bin
 fish_add_path /usr/local/sbin
-
-if type -q code
-    set -fx EDITOR "code --wait"
-else if type -q hx
-    set -gx EDITOR hx
-else if type -q vim
-    set -gx EDITOR vim
-else
-    set -gx EDITOR vi
-end
