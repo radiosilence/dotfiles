b3401b5ee9dfe588640aa55013a0e0433a2a635c
James Cleveland <jc@blit.cc>
2025-01-20 11:42:42 +0000

    small teething issues

--- config/fish/conf.d/brew.fish
+++ config/fish/conf.d/brew.fish
@@ -1,3 +1,11 @@
+if test -d /opt/homebrew
+  set -gx BREW_PREFIX /opt/homebrew
+else
+  set -gx BREW_PREFIX /usr/local
+end
+
+fish_add_path "$BREW_PREFIX/bin"
+
 using brew || exit
 set -gx HOMEBREW_BUNDLE_FILE "~/Brewfile"
 alias bb "brew bundle"
