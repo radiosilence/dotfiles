29bd52c0ded961148df342909cbe6bfa26905085
James Cleveland <jc@blit.cc>
2025-01-20 12:02:45 +0000

    more brew paths

--- config/fish/conf.d/brew.fish
+++ config/fish/conf.d/brew.fish
@@ -1,10 +1,11 @@
 if test -d /opt/homebrew
-  set -gx BREW_PREFIX /opt/homebrew
+    set -gx BREW_PREFIX /opt/homebrew
 else
-  set -gx BREW_PREFIX /usr/local
+    set -gx BREW_PREFIX /usr/local
 end
 
 fish_add_path "$BREW_PREFIX/bin"
+fish_add_path "$BREW_PREFIX/sbin"
 
 using brew || exit
 set -gx HOMEBREW_BUNDLE_FILE "~/Brewfile"
