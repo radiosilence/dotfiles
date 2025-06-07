eec0a9cf5e57efc32f14bb9dc9e6d5e28003149c
James Cleveland <jc@blit.cc>
2025-06-07 11:59:45 +0100

    various gardening

---
+++ config/fish/conf.d/local-bin.fish
@@ -0,0 +1,2 @@
+
+fish_add_path ~/.local/bin
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,7 @@
-fish_add_path /Users/james.cleveland/workspace/microsoft/typescript-go/built/local/
-# Added by Windsurf
-fish_add_path /Users/james.cleveland/.codeium/windsurf/bin
+
 
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
 # Added by Windsurf
-fish_add_path /Users/james.cleveland/.codeium/windsurf/bin
