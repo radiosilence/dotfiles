74459ab006514ce8441107c52f17788785e9a760
James Cleveland <jc@blit.cc>
2025-06-13 13:14:26 +0100

    dont do pnpm all the time

--- config/fish/conf.d/aws.fish
+++ config/fish/conf.d/aws.fish
@@ -1,4 +1,5 @@
-alias aws-shell='aws-vault exec -d 1000h -n'
+using aws-vault || exit
+alias aws-shell='aws-vault exec -d 72h -n'
 
 # login to AWS console
-alias aws-login='aws-vault login -d 1000h'
+alias aws-login='aws-vault login -d 72h'
