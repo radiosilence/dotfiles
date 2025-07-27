e666d717f7299c2995af92cb37e974ae5cdb8240
James Cleveland <jc@blit.cc>
2025-07-27 11:32:45 +0100

    sudo binary override to always use askpass

--- config/fish/conf.d/sudo-askpass.fish
+++ config/fish/conf.d/sudo-askpass.fish
@@ -3,4 +3,6 @@
 # Set up askpass - script auto-calculates item name from hostname/user
 # OP_SUDO_ITEM can override the default naming
 set -gx SUDO_ASKPASS ~/.dotfiles/bin/sudo-ask-pass
-alias sudo='sudo --askpass'
+
+# The sudo wrapper script in bin/sudo handles all sudo calls
+# No alias needed since bin/ is in PATH before /usr/bin
