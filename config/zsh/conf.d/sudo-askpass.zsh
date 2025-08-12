efb35a785d4c3805786ab5861d28096db8d20b09
James Cleveland <jc@blit.cc>
2025-08-12 09:55:16 +0100

    it broke everything

--- config/zsh/conf.d/sudo-askpass.zsh
+++
@@ -1,8 +0,0 @@
-# sudo-askpass - Configure sudo to use 1Password for password prompts
-
-# Set up askpass - script auto-calculates item name from hostname/user
-# OP_SUDO_ITEM can override the default naming
-export SUDO_ASKPASS=~/.dotfiles/bin/sudo-ask-pass
-
-# The sudo wrapper script in bin/sudo handles all sudo calls
-# No alias needed since bin/ is in PATH before /usr/bin
