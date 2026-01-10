76f3c21bd0d1ae27ebeb5063a5a2505783268d5d
James Cleveland <jc@blit.cc>
2026-01-10 15:57:55 +0000

    more clauding

---
+++ config/zsh/conf.d/utils.zsh
@@ -0,0 +1,19 @@
+# Utility aliases and functions for common tasks
+
+# Find what's using a port
+whatport() { lsof -i :$1 }
+
+# Show recently modified files (default: last hour)
+recent() { fd --type f --changed-within ${1:-1h} | head -20 }
+
+# Quick disk usage breakdown
+alias sizes='dust -d 2'
+
+# Show all listening ports
+alias listening='lsof -iTCP -sTCP:LISTEN -P -n'
+
+# Grep processes with tree view
+psg() { procs --tree | grep -i $1 }
+
+# Pretty git log
+alias glog='git log --oneline --graph --decorate -20'
