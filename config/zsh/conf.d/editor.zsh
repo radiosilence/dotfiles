e7c6d84e8e2781e88b359a0616598fc96aee5651
James Cleveland <jc@blit.cc>
2026-01-13 14:03:36 +0000

    prefer zed --wait as EDITOR

--- config/zsh/conf.d/editor.zsh
+++ config/zsh/conf.d/editor.zsh
@@ -1,5 +1,7 @@
 # Editor configuration
-if command -v hx >/dev/null; then
+if command -v zed >/dev/null; then
+  export EDITOR='zed --wait'
+elif command -v hx >/dev/null; then
   export EDITOR=hx
 elif command -v vim >/dev/null; then
   export EDITOR=vim
