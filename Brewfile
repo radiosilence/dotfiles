23b4b5e19305d17708fbe710e6f6c9cab4972154
James Cleveland <jc@blit.cc>
2026-02-19 10:53:26 +0000

    remove update-ffmpeg shit

--- Brewfile
+++ Brewfile
@@ -214,3 +214,4 @@ cask 'whatsapp', greedy: true
 # mas 'Adobe Lightroom', id: 1_451_544_217
 # mas 'Infuse', id: 1_136_220_934
 
+brew "ffmpeg"
--- crates/src/lib.rs
+++ crates/src/lib.rs
@@ -4,6 +4,5 @@ pub mod audio;
 pub mod install;
 pub mod parallel;
 pub mod regen_completions;
-pub mod update_ffmpeg;
 
 pub use anyhow::{Context, Result};
