3fb653eb223f5e25259cba5c87c489e0d5b523a2
James Cleveland <jc@blit.cc>
2026-03-13 10:43:57 +0000

    move my claude stuff in front of omc so it maybe listens

--- brewfiles.d/browsers.rb
+++ brewfiles.d/browsers.rb
@@ -2,4 +2,4 @@
 cask 'firefox', greedy: true
 cask 'zen', greedy: true
 cask 'browser-schedule', greedy: true, no_quarantine: true
-cask 'librewolf', greedy: true
+cask 'librewolf', greedy: true, no_quarantine: true
