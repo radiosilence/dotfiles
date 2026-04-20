9a12f6bd00ede5a7c69b1987c18ad53d5b8478cd
James Cleveland <jc@blit.cc>
2026-04-20 10:32:15 +0100

    various ting

--- config.d/zsh/conf.d/aws.zsh
+++ config.d/zsh/conf.d/aws.zsh
@@ -1,6 +1,3 @@
 # AWS configuration
-command -v aws-vault >/dev/null || return
-
-alias aws-shell='aws-vault exec -d 72h -n'
-# Login to AWS console
-alias aws-login='aws-vault login -d 72h'
+command -v REDACTED >/dev/null && alias aws-shell='REDACTED aws-shell'
+command -v aws-vault >/dev/null && alias aws-login='aws-vault login -d 72h'
