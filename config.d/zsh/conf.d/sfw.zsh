476b2efc41e974283cb4fa555793ffffe70120ba
James Cleveland <jc@blit.cc>
2026-07-28 12:32:54 +0100

    remove sfw free from zsh

--- config.d/zsh/conf.d/sfw.zsh
+++
@@ -1,8 +0,0 @@
-
-# Socket Firewall wraps for non-JS package managers ni doesn't touch.
-# sfw also supports: yarn, pnpm, bun, pip, uv, cargo (ni handles npm-family).
-if command -v sfw >/dev/null; then
-  alias uv='sfw uv'
-  alias uvx='sfw uvx'
-  alias cargo='sfw cargo'
-fi
